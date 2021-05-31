87e944ce6cb5a2936d8ea39801684c7cddb32239
chahayeong <c6prince@naver.com>
2021-05-31 17:56:42 +0900

    titanic_02

--- titanic/views/controller.py
+++ titanic/views/controller.py
@@ -7,14 +7,20 @@ class Controller(object):
     dataset = Dataset()
     service = Service()
 
-    def modeling(self, train, test) -> object:
+    def modeling(self, train, test):
         service = self.service
+        this = self.preprocess(train, test)
+        this.label = service.create_label(this)
+        this.train = service.create_train(this)
+        return this
 
-    def preprocess(self, train) -> object:
+    def preprocess(self, train, test):
         service = self.service
         this = self.dataset
         this.train = service.new_model(train)
+        this.test = service.new_model(test)
         print(f'Train 의 type은 {type(this.train)} 이다.')
-        print(f'Train 의 column 은 {this.train.columns} 이다.')
-        print(f'Train 의 상위 5개 데이터는 {this.train.head()} 이다.')
-        print(f'Train 의 하위 5개 데이터는 {this.train.tail()} 이다.')
+        print(f'Train 의 column 은 {this.test.columns} 이다.')
+        print(f'Test 의 상위 5개 데이터는 {this.train} 이다.')
+        print(f'Test 의 하위 5개 데이터는 {this.test.columns} 이다.')
+        return this
